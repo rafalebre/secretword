9d0958f48d434d872d7e49924e2dcb7a410d7b9b
Rafael Lebre <rafaellebre100@gmail.com>
2023-04-02 17:42:37 +0200

    Aula 72 - Escolhendo palavra e categoria aleatoriamente

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import { useCallback, useEffect, useState } from "react";
 // data
 import { wordsList } from './data/words';
 
+
 // components
 import StartScreen from './components/StartScreen';
 import Game from './components/Game';
@@ -22,10 +23,45 @@ const stages = [
 function App() {
   const [gameStage, setGameStage] = useState(stages[0].name);
   const [words] = useState(wordsList)
+
+  const [pickedWord, setPickedWord] = useState("");
+  const [pickedCategory, setPickedCategory] = useState ("");
+  const [letters, setLetters] = useState([]);
+
+  const pickWordAndCategory = () => {
+    // pick a random category
+  const categories = Object.keys(words)
+  const category = categories[Math.floor(Math.random() * Object.keys(categories).length)]
+
+  console.log(category);
+
+  // pick a random word
+  const word = words[category][Math.floor(Math.random() * words[category].length)];
+
+  console.log(word);
+
+  return { word, category };
+  };
   
 // starts the secret word game
 const startGame = () => {
-  setGameStage(stages[1].name)
+  // pick word and pick category
+  const { word, category } = pickWordAndCategory();
+
+  // create an array of letters
+  let wordLetters = word.split("");
+
+  wordLetters = wordLetters.map((l) => l.toLowerCase())
+
+  console.log(word, category);
+  console.log(wordLetters);
+
+  // fill states
+  setPickedWord(word);
+  setPickedCategory(category);
+  setLetters(letters);
+
+  setGameStage(stages[1].name);
 }
 
 // process the letter input
