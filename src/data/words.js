1781d1836281319739f30f7c2d9ff14a8662ea4e
Rafael Lebre <rafaellebre100@gmail.com>
2023-04-02 22:22:14 +0200

    Added animals and car brands

--- src/data/words.js
+++ src/data/words.js
@@ -1,9 +1,11 @@
 export const wordsList = {
-    car: ["Engine", "Doors", "Brakes", "Battery", "Trnasmission"],
+    carparts: ["Engine", "Doors", "Brakes", "Battery", "Transmission"],
     fruits: ["Pomegranate", "Pineapple", "Peach", "Tangerine", "Banana"],
     body: ["Head", "Shoulder", "Brain", "Neck", "Feet"],
     computer: ["Mouse", "Keyboard", "Monitor", "Motherboard"],
     programming: ["Language", "Framework", "JavaScript", "React"],
     food: ["Rice", "Beans", "Meet", "Milk", "Eggs"],
+    animals: ["Cheetah", "Leopard", "Tortoise", "Lion", "Bear"],
+    carbrands: ["BMW", "Audi", "Ferrari", "Bentley", "Porsche"]
   };
   
