e8003a83f5ee9ba65c7169c72c73a8586ec58ce1
Rafael Lebre <rafaellebre100@gmail.com>
2023-04-02 17:51:08 +0200

    Aula 73 - Estruturando componente do jogo

--- src/components/Game.js
+++ src/components/Game.js
@@ -2,9 +2,30 @@ import "./Game.css";
 
 const Game = ({verifyLetter}) => {
   return (
-    <div>
-        <h1>Game</h1>
-        <button onClick={verifyLetter}>Finish Game</button>
+    <div className="game">
+        <p className="points">
+            <span>Points: 000</span>
+        </p>
+        <h1>Guess the word</h1>
+        <h3 className="tip">
+            Tips about the word: <span>Tip...</span>
+        </h3>
+        <div className="wordContainer">
+            <span className="letter">A</span>
+            <span className="blankSquare"></span>
+        </div>
+        <div className="letterContainer">
+            <p>Try to guess a letter of the word:</p>
+            <form>
+                <input type="text" name="letter" maxLength="1" required />
+                <button>Play!</button>
+            </form>
+        </div>
+        <div className="wrongLettersContainer">
+            <p>Letters already used:</p>
+            <span>a,</span>
+            <span>b,</span>
+        </div>
     </div>
   )
 }
